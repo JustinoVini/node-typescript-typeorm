8aa0eeed7279c4bfb8f36f6a2eefbfa4fd22868a
Vini <visilva5690@hotmail.com>
2024-02-19 22:03:47 -0300

    Feat: Criando a primeira Rota do sistema

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,16 @@
 import express from "express";
 import cors from "cors";
+import routes from './routes/index';
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+const PORT = 3200;
+
+app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}! 🏆`);
+});
